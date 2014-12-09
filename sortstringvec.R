806d9996ceea8a2508f54accce1bc4a9dc9fde5b
James Peterson <James.Peterson@nih.gov>
2014-12-08 20:58:22 -0500

    First commit of sortstringvec.R

---
+++ sortstringvec.R
@@ -0,0 +1,6 @@
+sortstringvec <- function(stringvec) {
+  ## Takes as input a vector of strings and outputs the vector of strings, sorted
+  ## in lexicographic order.
+  return(sort(stringvec))
+}
+  
