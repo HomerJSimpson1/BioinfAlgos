c3ebba86a84d8b50779c70d8d1f89f70299804fa
James Peterson <james@recruitcom.com>
2014-12-10 23:55:43 -0500

    Latest update

--- debruijn.R
+++ debruijn.R
@@ -1,47 +1,70 @@
 debruijn <- function(k, text) {
   ## Solve the DeBruijn Graph from a string Problem
-  numpatterns <- length(patterns)
-
-  patterns <- sort(patterns)
-  prefixes <- vector(mode='character', length=numpatterns)
-  suffixes <- vector(mode='character', length=numpatterns)
-
-  for (i in 1:numpatterns) {
-    ## For each pattern in patterns, get the prefix and suffix of the pattern, where
-    ## the prefix is the first (k - 1) symbols of pattern, and the suffix is the last
-    ## (k - 1) symbols of the pattern. e.g if pattern = "ATAT" then prefix = "ATA" and
-    ## suffix = "TAT".
-    prefixes[i] <- getkminus1(patterns[i], "first")
-    suffixes[i] <- getkminus1(patterns[i], "last")    
-  }
-
-  adjacencylist <- vector(mode='list', length=numpatterns)
   
-  ## Iterate through the patterns list again and find where each prefix matches each suffix.
-  ## If they match, add the suffix of the matching pair to the adjacency list of the prefix.
-  ## The length of prefixes and suffixes should be the same as the length of patterns, and
-  ## I used the variable "numpatterns" to store the value (to save on repeated calls to
-  ## length(patterns).
-  for (i in 1:numpatterns) {
-    suf <- suffixes[i]
+  patterns <- composition(k, text)
+  print(patterns)
 
-    temp <- vector('character')
-    for (j in 1:numpatterns) {
-      if (suf == prefixes[j]) {        
-        ## Add it to the adjacency list of patterns[i]
-        temp <- c(temp, patterns[j])                
-      } ## end if (pre == suffixes[j])        
-    } ## end for (j in 1:length(suffixes))
-
-    adjacencylist[[i]] <- c(adjacencylist[[i]], temp)        
-  } ## end for (i in 1:numpatterns)
-
-  graph <- as.data.frame(cbind(patterns, prefixes, suffixes, adjacencylist), stringsAsFactors=FALSE)
   
-  outstring <- printgraph(graph)
-  writestringtofile("overlapgraphout.txt", outstring)
-  return(graph)
+##   numpatterns <- length(patterns)
+
+##   patterns <- sort(patterns)
+##   prefixes <- vector(mode='character', length=numpatterns)
+##   suffixes <- vector(mode='character', length=numpatterns)
+
+##   for (i in 1:numpatterns) {
+##     ## For each pattern in patterns, get the prefix and suffix of the pattern, where
+##     ## the prefix is the first (k - 1) symbols of pattern, and the suffix is the last
+##     ## (k - 1) symbols of the pattern. e.g if pattern = "ATAT" then prefix = "ATA" and
+##     ## suffix = "TAT".
+##     prefixes[i] <- getkminus1(patterns[i], "first")
+##     suffixes[i] <- getkminus1(patterns[i], "last")    
+##   }
+
+##   adjacencylist <- vector(mode='list', length=numpatterns)
+  
+##   ## Iterate through the patterns list again and find where each prefix matches each suffix.
+##   ## If they match, add the suffix of the matching pair to the adjacency list of the prefix.
+##   ## The length of prefixes and suffixes should be the same as the length of patterns, and
+##   ## I used the variable "numpatterns" to store the value (to save on repeated calls to
+##   ## length(patterns).
+##   for (i in 1:numpatterns) {
+##     suf <- suffixes[i]
+
+##     temp <- vector('character')
+##     for (j in 1:numpatterns) {
+##       if (suf == prefixes[j]) {        
+##         ## Add it to the adjacency list of patterns[i]
+##         temp <- c(temp, patterns[j])                
+##       } ## end if (pre == suffixes[j])        
+##     } ## end for (j in 1:length(suffixes))
+
+##     adjacencylist[[i]] <- c(adjacencylist[[i]], temp)        
+##   } ## end for (i in 1:numpatterns)
+
+##   graph <- as.data.frame(cbind(patterns, prefixes, suffixes, adjacencylist), stringsAsFactors=FALSE)
+  
+##   outstring <- printgraph(graph)
+##   writestringtofile("overlapgraphout.txt", outstring)
+##   return(graph)
 }
+
+
+
+
+composition <- function(k, text) {
+  ## Split the given text into all possible strings of length k
+  ## and return the fragments in lexicographic order.
+  textlen <- nchar(text)
+  fragments <- character()
+  
+  for (i in 1:(textlen - k + 1)) {
+    nextstr <- substr(text, i, i+k-1)
+    fragments <- append(fragments, nextstr)
+  }
+  
+  return(sort(fragments))
+}
+
 
 
 
@@ -102,4 +125,33 @@ writestringtofile <- function(filename, string) {
   writeLines(c(string), fileConn)
   close(fileConn)
 
+}
+
+
+
+
+
+
+
+stringgenomepath <- function(patterns) {
+  ## Input:  A sequence of k-mers Pattern1, бн ,Patternn such that the last k - 1
+  ##         symbols of Patterni are equal to the first k-1 symbols of Patterni+1
+  ##         for 1 <= i <= n-1.
+  ## Output: A string Text of length k+n-1 such that the i-th k-mer in Text is equal
+  ##         to Patterni  (for 1 <= i <= n)
+
+  ## Add the first element of the patterns vector
+  sequence <- patterns[1]
+  #print(sequence)
+  ## For the remainder of the patterns vector, add the last symbol of each pattern
+  for (i in 2:length(patterns)) {
+    #print(patterns[i])
+    sequence <- c(sequence, lastsymbol(patterns[i]))
+  }
+
+  ## Collapse into a string
+  sequence <- paste(sequence, "", sep="", collapse="")
+
+  ## Return the result
+  return(sequence)
 }
