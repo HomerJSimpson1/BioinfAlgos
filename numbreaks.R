f1d7c5b5e1bbfe8185490758e85a9c5c81507401
James Peterson <James.Peterson@nih.gov>
2015-01-15 17:05:33 -0500

    Latest updates numbreaks.R

--- numbreaks.R
+++ numbreaks.R
@@ -1,5 +1,6 @@
-numbreaks <- function(Pstring) {
-  ## Input: A permutation
+numbreaks <- function(Pstring="", isfile=FALSE, fname="") {
+  ## Input: A permutation.  If isfile == TRUE, then Pstring designates a file containing Pstring (the permutation).
+  ##        If isfile == FALSE, then Pstring is the raw string representing the permutation.
   ## Output: The number of breakpoints in this permutation.
 
   ## Sample Input:
@@ -8,18 +9,55 @@ numbreaks <- function(Pstring) {
   ## Sample Output:
   ##    8
 
-  ## Convert Pstring into an integer vector P (see the greedysorting.R file)
-  P <- readP(Pstring)
+  ## if the permutation needs to be read from a file, then read in the file into Pstring
+  if (isfile) {
+    fname <- Pstring
+    Pstring1 <- readfiletostring(fname)
+    ## Convert Pstring into an integer vector P (see the greedysorting.R file)
+    P <- readP(Pstring1)
+  }
+  else   ## Convert Pstring into an integer vector P (see the greedysorting.R file)
+    P <- readP(Pstring)
+
+  ##print(str(Pstring1))
+    
+  ## ## Convert Pstring into an integer vector P (see the greedysorting.R file)
+  ##P <- readP(Pstring)
 
   ## Add a "0" element and an "n + 1" element
+  #print(max(P))
   P1 <- c(0, P, length(P) + 1)
+  #print(P1)  
 
+  print(length(P))
+  print(length(P1))
+  
   ## Create a grouping of each pair of adjacent elements to see if they are an adjacency or a breakpoint.
   ## Or just do it in place in the vector?
+  adjbreak <- vector('character')
+  for (i in 1:(length(P1) - 1)) {
+    ## iterate through the P1 vector and compare each two adjacent elements to determine if
+    ## if they are an adjacency or a break.
+    #print(P1[i])
+    #print(P1[i + 1])
+    if (P1[i+1] == (P1[i] + 1)) {
+      adjbreak <- c(adjbreak, 'adj')
+    }
+    else {
+      adjbreak <- c(adjbreak, 'break')
+    }
+    
+  } ## end for (i in 1:(length(P1) - 1))
 
-  print(P1)
+  breakpoints <- adjbreak[adjbreak=='break']
+  adjacencies <- adjbreak[adjbreak=='adj']
+  print(adjacencies)
 
-  breakpoints <- 0
+  numbrk <- length(breakpoints)
+  numadj <- length(adjacencies)
+
+  print(numbrk)
+  print(numadj)
   
   return(breakpoints)
 }
@@ -42,6 +80,35 @@ printP <- function(P, fname) {
 
 readP <- function(input) {
   ## Convert input into a vector
-  temp <- as.integer(strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]])  
+  temp <- as.integer(strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]])
   return(temp)
+}
+
+
+
+
+## readfiletovec <- function(filename, header=FALSE, stringsAsFactors=FALSE) {
+##   ## Read the contents of a file into a vector
+##   ## Each line in the file corresponds to a single element of the vector.
+##   vec <- read.table(filename, header=header, stringsAsFactors=stringsAsFactors)
+##   return(vec[,1])
+## }
+
+
+
+
+readfiletostring <- function(filename, usespace=FALSE, nocollapse=FALSE) {
+  ## Read the contents of a file into a single string.
+  if(usespace)
+    if(nocollapse)
+      result <- paste(readLines(filename))
+    else
+      result <- paste(readLines(filename), collapse=" ")
+  else
+    if(nocollapse)
+      result <- paste(readLines(filename))
+    else
+      result <- paste(readLines(filename), collapse="")
+
+  return(result)
 }
