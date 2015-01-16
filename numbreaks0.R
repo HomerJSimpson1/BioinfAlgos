7ada62e9b3259a2db35e6c0361d9c6072ba4ed23
James Peterson <james@recruitcom.com>
2015-01-15 20:49:23 -0500

    Final numbreaks.R versions.

--- numbreaks0.R
+++ numbreaks0.R
@@ -11,6 +11,9 @@ numbreaks <- function(Pstring="", isfile=FALSE, fname="") {
 
   ## if the permutation needs to be read from a file, then read in the file into Pstring
   if (isfile) {
+    #fname <- Pstring
+    #Pstring1 <- readfiletostring(fname)
+    #print(fname)
     Pstring1 <- readfiletostring(fname, usespace=TRUE, nocollapse=TRUE)    
     ## Convert Pstring into an integer vector P (see the greedysorting.R file)
     P <- readP(Pstring1)
@@ -18,15 +21,27 @@ numbreaks <- function(Pstring="", isfile=FALSE, fname="") {
   else   ## Convert Pstring into an integer vector P (see the greedysorting.R file)
     P <- readP(Pstring)
 
-  ## Add a "0" element and an "n + 1" element
-  P1 <- c(0, P, length(P) + 1)
+  ##print(str(Pstring1))
+    
+  ## ## Convert Pstring into an integer vector P (see the greedysorting.R file)
+  ##P <- readP(Pstring)
 
+  ## Add a "0" element and an "n + 1" element
+  #print(max(P))
+  P1 <- c(0, P, length(P) + 1)
+  #print(P1)  
+
+  print(length(P))
+  print(length(P1))
+  
   ## Create a grouping of each pair of adjacent elements to see if they are an adjacency or a breakpoint.
   ## Or just do it in place in the vector?
   adjbreak <- vector('character')
   for (i in 1:(length(P1) - 1)) {
     ## iterate through the P1 vector and compare each two adjacent elements to determine if
     ## if they are an adjacency or a break.
+    #print(P1[i])
+    #print(P1[i + 1])
     if (P1[i+1] == (P1[i] + 1)) {
       adjbreak <- c(adjbreak, 'adj')
     }
@@ -38,15 +53,17 @@ numbreaks <- function(Pstring="", isfile=FALSE, fname="") {
 
   breakpoints <- adjbreak[adjbreak=='break']
   adjacencies <- adjbreak[adjbreak=='adj']
+  #print(adjacencies)
 
   numbrk <- length(breakpoints)
   numadj <- length(adjacencies)
 
-  print(paste("Number of breakpoints =", numbrk, sep=" "))
-  print(paste("Number of adjacencies =", numadj, sep=" "))
+  print(numbrk)
+  print(numadj)
   
   return(breakpoints)
 }
+
 
 
 
@@ -66,6 +83,7 @@ printP <- function(P, fname) {
 readP <- function(input) {
   ## Convert input into a vector
   temp <- strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]]
+  print(length(temp))
   temp1 <- as.integer(temp)
   
   ##temp <- as.integer(strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]])
