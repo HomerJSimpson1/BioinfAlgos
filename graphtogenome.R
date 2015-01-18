c90b46dd10828744f01058bcd1459a459828b791
James Peterson <james@recruitcom.com>
2015-01-17 21:29:23 -0500

    First commit graphtogenome.R

---
+++ graphtogenome.R
@@ -0,0 +1,106 @@
+graphtogenome <- function(genomegraph) {
+  ## Input:  The colored edges ColoredEdges of a genome graph.
+  ## Output: The genome P corresponding to this genome graph.
+
+  nodes <- vector('numeric')
+  genomevec <- splitchroms(genomegraph)
+  mylist <- list()
+  
+  for (i in 1:length(genomevec)) {
+    item <- genomevec[i]
+    items <- strsplit(item, ",")[[1]]
+    items <- as.integer(gsub("[[:punct:]]", "", items))
+    nodes <- c(nodes, items[1], items[2])
+    if (items[1] > items[2]) {
+      ## Then we've hit the end of the cycle
+      nodes <- c(nodes[length(nodes)], nodes[1:(length(nodes) - 1)])
+      strnodes <- paste(nodes, "", sep=" ", collapse="")
+      strnodes <- substr(strnodes, 1, nchar(strnodes) - 1)
+      strnodes <- paste("(", strnodes, ")", sep="")
+      mylist <- c(mylist, strnodes)
+      strnodes <- ""
+      nodes <- vector('numeric')
+    } ## end if (items[1] > items[2])
+
+  } ## end for (i in 1:length(genomevec))
+
+
+  P <- ""
+  for (i in 1:length(mylist)) {
+    chromo <- cycletochromo(mylist[[i]])
+    chro <- printP(chromo, tofile=FALSE, showsign=TRUE)
+    P <- paste(P, chro, sep=" ")    
+  }
+
+  P <- substr(P, 2, nchar(P))
+  return(P)
+}
+
+
+
+
+
+splitchroms <- function(strgenome) {
+  ## Separate the chromosomes in a genome provided as a string.
+  chroms <- strsplit(strgenome, "), ")[[1]]
+  chroms1 <- sapply(chroms, paste, ")", sep="")
+  chroms1[length(chroms1)] <- substr(chroms1[length(chroms1)], 1, nchar(chroms1[length(chroms1)]) - 1)
+  chroms1 <- unname(chroms1)
+  
+  return(chroms1)
+}
+
+
+
+
+cycletochromo <- function(strnodes) {
+  ## Transform a cyclic representation into an adjacency list representation.
+  ## Input:  A sequence Nodes of integers between 1 and 2n.
+  ## Output: The chromosome Chromosome containing n synteny blocks resulting from
+  ##         applying cyletochromo to "nodes."
+
+  nodes <- readP(strnodes)  
+  chromlen <- length(nodes) / 2
+  chromo <- vector(mode = 'numeric', length = chromlen)
+  
+  for (j in 1:chromlen) {
+    if (nodes[(2*j) - 1] < nodes[2*j])
+      chromo[j] <- nodes[2*j] / 2
+    else
+      chromo[j] <- -(nodes[((2*j) - 1)] / 2)
+  } ## end for (j in 1:chromlen)
+
+  printP(chromo, tofile=FALSE, showsign=TRUE)
+
+  return(chromo)
+}
+
+
+
+
+readP <- function(input) {
+  ## Convert input into a vector
+  temp <- as.integer(strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]])  
+  return(temp)
+}
+
+
+
+
+printP <- function(P, fname="", tofile=TRUE, showsign=TRUE) {
+  ## Print the P vectorlines to a file
+  if(showsign) {
+    nums <- paste(sprintf("%+d", P), "", collapse="")
+    trimd <- substr(nums, 1, nchar(nums) - 1)
+  }
+  else {
+    nums <- paste(sprintf("%d", P), "", collapse="")
+    trimd <- substr(nums, 1, nchar(nums) - 1)
+  }
+  strOut <- paste("(", trimd, ")", sep="")
+    
+##   if (tofile)
+##     write(strOut, fname, append=TRUE)
+##   else
+##     print(strOut)
+}
---
+++ graphtogenome1.R
@@ -0,0 +1,130 @@
+graphtogenome1 <- function(genomegraph) {
+  ## Input:  The colored edges ColoredEdges of a genome graph.
+  ## Output: The genome P corresponding to this genome graph.
+
+  #P <- vector('character')
+  nodes <- vector('numeric')
+  genomevec <- splitchroms(genomegraph)
+  mylist <- list()
+  
+  for (i in 1:length(genomevec)) {
+    item <- genomevec[i]
+    items <- strsplit(item, ",")[[1]]
+
+    #items <- gsub('(', "", items)    
+    #items <- gsub('\)', "", items)
+    items <- as.integer(gsub("[[:punct:]]", "", items))
+    #print(items)
+    nodes <- c(nodes, items[1], items[2])
+    if (items[1] > items[2]) {
+      ## Then we've hit the end of the cycle
+      nodes <- c(nodes[length(nodes)], nodes[1:(length(nodes) - 1)])
+
+      #print(nodes)
+      strnodes <- paste(nodes, "", sep=" ", collapse="")
+      strnodes <- substr(strnodes, 1, nchar(strnodes) - 1)
+      strnodes <- paste("(", strnodes, ")", sep="")
+      #print(strnodes)
+      mylist <- c(mylist, strnodes)
+      strnodes <- ""
+##       ##strnodes <- paste("(", paste(nodes, "", sep=" ", collapse=""), ")", sep="")
+##       #print(strnodes)
+##       chromo <- cycletochromo(strnodes)
+##       chro <- printP(chromo, tofile=FALSE, showsign=TRUE)
+##       #print(chro)
+##       #P <- c(P, chromo)
+##       P <- paste(P, chro, sep=", ")
+      nodes <- vector('numeric')
+    } ## end if (items[1] > items[2])
+
+  } ## end for (i in 1:length(genomevec))
+
+
+  P <- ""
+  for (i in 1:length(mylist)) {
+    #print(mylist[[i]])
+    chromo <- cycletochromo(mylist[[i]])
+    #print(chromo)
+    chro <- printP(chromo, tofile=FALSE, showsign=TRUE)
+    #print(chro)
+    #P <- paste(P, chro, sep=", ")
+    P <- paste(P, chro, sep=" ")    
+    #print(P)
+  }
+
+  #P <- substr(P, 3, nchar(P))
+  P <- substr(P, 2, nchar(P))  
+
+  #return(mylist)  
+  return(P)
+}
+
+
+
+
+
+splitchroms <- function(strgenome) {
+  ## Separate the chromosomes in a genome provided as a string.
+  chroms <- strsplit(strgenome, "), ")[[1]]
+  #chroms <- substr(chroms, 1, nchar(chroms) - 1)
+  chroms1 <- sapply(chroms, paste, ")", sep="")
+  chroms1[length(chroms1)] <- substr(chroms1[length(chroms1)], 1, nchar(chroms1[length(chroms1)]) - 1)
+  chroms1 <- unname(chroms1)
+  
+  return(chroms1)
+}
+
+
+
+
+cycletochromo <- function(strnodes) {
+  ## Transform a cyclic representation into an adjacency list representation.
+  ## Input:  A sequence Nodes of integers between 1 and 2n.
+  ## Output: The chromosome Chromosome containing n synteny blocks resulting from
+  ##         applying cyletochromo to "nodes."
+
+  nodes <- readP(strnodes)  
+  chromlen <- length(nodes) / 2
+  chromo <- vector(mode = 'numeric', length = chromlen)
+  
+  for (j in 1:chromlen) {
+    if (nodes[(2*j) - 1] < nodes[2*j])
+      chromo[j] <- nodes[2*j] / 2
+    else
+      chromo[j] <- -(nodes[((2*j) - 1)] / 2)
+  } ## end for (j in 1:chromlen)
+
+  printP(chromo, tofile=FALSE, showsign=TRUE)
+
+  return(chromo)
+}
+
+
+
+
+readP <- function(input) {
+  ## Convert input into a vector
+  temp <- as.integer(strsplit(substr(input, 2, nchar(input) - 1), " ")[[1]])  
+  return(temp)
+}
+
+
+
+
+printP <- function(P, fname="", tofile=TRUE, showsign=TRUE) {
+  ## Print the P vectorlines to a file
+  if(showsign) {
+    nums <- paste(sprintf("%+d", P), "", collapse="")
+    trimd <- substr(nums, 1, nchar(nums) - 1)
+  }
+  else {
+    nums <- paste(sprintf("%d", P), "", collapse="")
+    trimd <- substr(nums, 1, nchar(nums) - 1)
+  }
+  strOut <- paste("(", trimd, ")", sep="")
+    
+##   if (tofile)
+##     write(strOut, fname, append=TRUE)
+##   else
+##     print(strOut)
+}
